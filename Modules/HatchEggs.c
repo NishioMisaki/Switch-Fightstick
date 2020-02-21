b5d1b44ee863d35c467e02c9e4e3a5924c5fa06e
NishioMisaki <55392193+NishioMisaki@users.noreply.github.com>
2020-02-21 18:01:26 +0900

    Update HatchEggs.c

--- Modules/HatchEggs.c
+++ Modules/HatchEggs.c
@@ -160,17 +160,13 @@ static uint8_t PutPokemonInBox(USB_JoystickReport_Input_t* const ReportData, uin
 static uint8_t GetEgg(USB_JoystickReport_Input_t* const ReportData, uint16_t count)
 {
 	switch (count) {
-	case 0 ... 99:
+	case 0 ... 149:
 		/* Move to breeder */
 		ReportData->LY = STICK_MAX - 30;
 		break;
-	case  100 ... 109:
+	case  150 ... 169:
 		ReportData->LX = STICK_MAX;
-		break;
-	case 110 ... 169:
-		/* Move to breeder */
-		ReportData->LY = STICK_MAX - 30;
-		break;
+		break;		
 	case 150+20 ... 799+20:
 		/* Get egg */
 		if (count % 50 < 25)
