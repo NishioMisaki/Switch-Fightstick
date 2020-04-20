9e9f45c45f5f36c13764a1045644f0caa88d8511
NishioMisaki <55392193+NishioMisaki@users.noreply.github.com>
2020-04-20 22:01:14 +0900

    Update Modules.h

--- Modules/Modules.h
+++ Modules/Modules.h
@@ -15,5 +15,6 @@ extern void LoopTournament_Module(USB_JoystickReport_Input_t* const ReportData);
 extern void RepeatA_Module(USB_JoystickReport_Input_t* const ReportData);
 extern void ReleasePokemons_Module(USB_JoystickReport_Input_t* const ReportData);
 extern void LoopBattleTower_Module(USB_JoystickReport_Input_t* const ReportData);
+extern void GetMal_Module(USB_JoystickReport_Input_t* const ReportData);
 
 #endif
